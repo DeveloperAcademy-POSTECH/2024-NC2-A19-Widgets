6b9a1807e574b46caa7883b1ee2bd7f33d4d00b2
Jeong Dong <eejxdong99@gmail.com>
2024-06-17 11:16:34 +0900

    프로젝트 폴더 구조 정의

---
+++ NC2/NC2/Extension/UserDefaults+Ext.swift
@@ -0,0 +1,12 @@
+//
+//  UserDefaults+Ext.swift
+//  NC2
+//
+//  Created by 이정동 on 6/17/24.
+//
+
+import Foundation
+
+extension UserDefaults {
+    static let group: UserDefaults? = UserDefaults(suiteName: "group.nc2.widgets.com")
+}
---
+++ NC2/NC2/Model/EngWord.swift
@@ -0,0 +1,9 @@
+//
+//  EngWord.swift
+//  NC2
+//
+//  Created by 이정동 on 6/17/24.
+//
+
+import Foundation
+
