0ecd8170b3d3985cd11a6aa04e9680860e5946ad
Jeong Dong <eejxdong99@gmail.com>
2024-06-17 14:17:59 +0900

    Model 정의 및 json 파일 생성

--- NC2/NC2/Model/EngWord.swift
+++ NC2/NC2/Model/EngWord.swift
@@ -7,3 +7,24 @@
 
 import Foundation
 
+struct EngWord: Codable {
+    var word: String
+    var meaning: String
+    var partOfSpeech: String
+    var examples: [Sentence]
+    
+    enum CodingKeys: String, CodingKey {
+        case word, meaning, examples
+        case partOfSpeech = "part_of_speech"
+    }
+}
+
+struct Sentence: Codable {
+    var english: String
+    var korean: String
+    
+    enum CodingKeys: String, CodingKey {
+        case english = "en"
+        case korean = "ko"
+    }
+}
